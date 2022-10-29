d57d6ef3d6c2c2ec60948abc808892dcce3ac18e
Toops61 <toops61@gmail.com>
2022-10-29 19:22:35 +0200

    finish carte memoire game

--- 9. MemoryCard/app.js
+++ 9. MemoryCard/app.js
@@ -23,11 +23,13 @@ const flipCard = (e,index) => {
                 flippedCards[0].classList.add('same');
                 flippedCards[1].classList.add('same');
                 Array.from(cardDivArray).every(element => element.className.includes('same')) && winningAlert();
+            } else {
+                setTimeout(() => {
+                    flippedCards[0].classList.remove('flip');
+                    flippedCards[1].classList.remove('flip');
+                }, 1000);
             }
         }
-        setTimeout(() => {
-            !cardDivArray[index].className.includes('same') && cardDivArray[index].classList.remove('flip');
-        }, 1000);
     }
 }
 
@@ -44,7 +46,7 @@ document.querySelector('button').addEventListener('click',resetAll);
 
 const displayImages = array => {
     Array.from(document.querySelectorAll('.front img')).map((e,index) => {
-        e.setAttribute('src',`./ressources/${array[index]}.svg`);
+        e.setAttribute('src',`./ressources/${array[index]}`);
     })
 }
 
@@ -73,7 +75,7 @@ const buildCards = array => {
 const randomizeImages = () => {
     const newArrayImages = [];
     do {
-        const index = Math.floor(Math.random() * 6);
+        const index = Math.floor(Math.random() * arrayImages.length);
         newArrayImages.filter(e => e === arrayImages[index]).length < 2 && newArrayImages.push(arrayImages[index]);
     } while (newArrayImages.length < arrayImages.length*2);
     buildCards(newArrayImages);
@@ -82,9 +84,15 @@ const randomizeImages = () => {
 fetch('./ressources')
 .then(response => response.text())
 .then(data => {
-    arrayImages = data.split('ressources/').filter(e => e.includes('.svg'));
-    arrayImages = arrayImages.map(e => e.split('.svg')[0]);
-    arrayImages = arrayImages.filter(e => e !== 'question');
+    arrayImages = data.split('ressources/').filter(e => e.includes('.svg') || e.includes('.png') || e.includes('.jpg') || e.includes('.webp'));
+    arrayImages = arrayImages.map(e => {
+        e.includes('.webp') && (e = `${e.split('.webp')[0]}.webp`);
+        e.includes('.jpg') && (e = `${e.split('.jpg')[0]}.jpg`);
+        e.includes('.png') && (e = `${e.split('.png')[0]}.png`);
+        e.includes('.svg') && (e = `${e.split('.svg')[0]}.svg`);
+        return e;
+    });
+    arrayImages = arrayImages.filter(e => e !== 'question.svg');
     randomizeImages();
 })
 .catch(err => console.log(err));
