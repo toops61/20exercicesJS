893722053bcbf1eddb4b7d6a91f02be8c5f1507e
Toops61 <toops61@gmail.com>
2022-11-01 22:21:32 +0100

    password generate

--- 12. Generateur de mot de passe/app.js
+++ 12. Generateur de mot de passe/app.js
@@ -2,11 +2,20 @@ const passwordObject = {
     lowercase: false,
     uppercase: false,
     integers: false,
-    specials: false
+    specials: false,
+    length: 4
 }
 
 const copyAlert = () => {
     console.log('copié');
+}
+
+const alertWindow = text => {
+    document.querySelector('.alert-box h1').textContent = text;
+    document.querySelector('.alert-window').classList.remove('hide');
+    setTimeout(() => {
+        document.querySelector('.alert-window').classList.add('hide');
+    }, 2000);
 }
 
 const copyPassword = () => {
@@ -19,6 +28,7 @@ document.querySelector('.copy-icon').addEventListener('click',copyPassword);
 const passwordLength = e => {
     const length = e.target.value;
     document.querySelectorAll('label span')[0].textContent = length;
+    passwordObject.length = length;
 }
 
 document.querySelector('#length').addEventListener('input',e => passwordLength(e));
@@ -26,15 +36,48 @@ document.querySelector('#length').addEventListener('input',e => passwordLength(e
 const passwordParams = (e,index) => {
     const key = Object.keys(passwordObject)[index];
     passwordObject[key] = !passwordObject[key];
-    console.log(e.target.checked);
 }
 
-Array.from(document.querySelectorAll('.input-container input')).map((el,index) => {
+const inputsCheckedArray = Array.from(document.querySelectorAll('.input-container input'));
+
+inputsCheckedArray.map((el,index) => {
     el.addEventListener('input',e => passwordParams(e,index));
 })
 
-let codeArray = new Uint32Array(10);
-self.crypto.getRandomValues(codeArray);
-codeArray.map(e => {
-    console.log(e);
-})
+const generateRandomNumbers = lettersNumbersString => {
+    let codeArray = new Uint32Array(passwordObject.length);
+    self.crypto.getRandomValues(codeArray);
+    codeArray = codeArray.map(e => {
+        const newNumber = e / 4294967296;
+        const index = Math.floor(newNumber * lettersNumbersString.length);
+        return index;
+    })
+    return codeArray;
+}
+
+const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const uppercaseLetters = lowercaseLetters.toUpperCase();
+const allIntegers = '0123456789';
+const specialsCaracters = '#@&"\'(§!)-$€*£?.:=+/=<>';
+
+const concatenateCaracters = (array,string) => {
+    let password = '';
+    array.map(e => {
+        password += string[e];
+    })
+    return password;
+}
+
+const generatePassword = (e) => {
+    e.preventDefault();
+    if (inputsCheckedArray.some(e => e.checked)) {     
+        const lettersNumbersString = (passwordObject.lowercase ? lowercaseLetters : '') + (passwordObject.uppercase ? uppercaseLetters : '') + (passwordObject.integers ? allIntegers : '') + (passwordObject.specials ? specialsCaracters : '');
+        const array = generateRandomNumbers(lettersNumbersString);
+        const password =concatenateCaracters(array,lettersNumbersString);
+        document.querySelector('.result p').textContent = password;
+    } else {
+        alertWindow('Vous devez sélectionner au moins un paramètre');
+    }
+}
+
+document.querySelector('.button-generate').addEventListener('click',e => generatePassword(e));
